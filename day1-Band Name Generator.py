0a5c4136b4628fa3b78ee509b463efca070aeacb
Babang-tamvan341 <bangbung635@gmail.com>
2024-11-04 15:10:24 +0100

    day 1

---
+++ day1-Band Name Generator.py
@@ -0,0 +1,14 @@
+# this is 1st day of my 100 days python journal
+''' this what I've learned on my 1st day
+1. printing,
+2. string manipulation and code Intelligence
+3. Input function
+4. python variables
+5. variable naming '''
+# my first project is making "Band Name Generator"
+
+print ("welcome to Band Name Generator")
+city = input(f"please input your city \n ")
+pet = input(f"please input your pet name \n")
+print(f"there you go!!\nyour Band Name is {pet} {city} ")
+
